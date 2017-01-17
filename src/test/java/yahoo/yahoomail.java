6a233c31be096fe5c93dfef1f88c76506bcf4121
Swathi <Swathi@Swathi-PC>
2017-01-17 18:05:46 +0530

    yahoo commit

---
+++ src/test/java/yahoo/yahoomail.java
@@ -0,0 +1,16 @@
+package yahoo;
+
+import org.testng.annotations.Test;
+
+public class yahoomail {
+	
+	
+	@Test
+	
+	public void yahoomail()
+	{
+		System.out.println("Yahoo i am a winner");
+		
+	}
+
+}
